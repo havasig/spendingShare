5d54ff69512992d88206853cdfd530dcd975d5f0
Havasi Gabor <havasi.gabor@ursys.net>
2022-02-19 11:15:18 +0100

    reset to default project

--- lib/main.dart
+++ lib/main.dart
@@ -1,212 +1,115 @@
-import 'dart:ui';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:spending_share/translations.dart';
-
-import 'auth/login.dart';
-import 'auth/register.dart';
 
 void main() {
-  runApp(GetMaterialApp(
-    initialRoute: '/login',
-    translations: MyTranslations(),
-    locale: const Locale('en', 'EN'),
-    getPages: [
-      GetPage(name: '/login', page: () => Login()),
-      GetPage(
-        name: '/register',
-        page: () => Register(),
-      ),
-      GetPage(
-        name: '/home',
-        page: () => Home(),
-        binding: SampleBind(),
-      ),
-    ],
-  ));
+  runApp(const MyApp());
 }
 
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
-
-class Controller extends GetxController {
-  int count = 0;
-  void increment() {
-    count++;
-    // use update method to update all count variables
-    update();
-  }
-}
-
-
-class Home extends GetView<ControllerX> {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('second Route'),
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Obx(
-                  () {
-                print("count1 rebuild");
-                return Text('${controller.count1}');
-              },
-            ),
-            Obx(
-                  () {
-                print("count2 rebuild");
-                return Text('${controller.count2}');
-              },
-            ),
-            Obx(() {
-              print("sum rebuild");
-              return Text('${controller.sum}');
-            }),
-            Obx(
-                  () => Text('Name: ${controller.user.value.name}'),
-            ),
-            Obx(
-                  () => Text('Age: ${controller.user.value.age}'),
-            ),
-            ElevatedButton(
-              child: Text("Go to last page"),
-              onPressed: () {
-                Get.toNamed('/third', arguments: 'arguments of second');
-              },
-            ),
-            ElevatedButton(
-              child: Text("Back page and open snackbar"),
-              onPressed: () {
-                Get.back();
-                Get.snackbar(
-                  'User 123',
-                  'Successfully created',
-                );
-              },
-            ),
-            ElevatedButton(
-              child: Text("Increment"),
-              onPressed: () {
-                controller.increment();
-              },
-            ),
-            ElevatedButton(
-              child: Text("Increment"),
-              onPressed: () {
-                controller.increment2();
-              },
-            ),
-            ElevatedButton(
-              child: Text("Update name"),
-              onPressed: () {
-                controller.updateUser();
-              },
-            ),
-            ElevatedButton(
-              child: Text("Dispose worker"),
-              onPressed: () {
-                controller.disposeWorker();
-              },
-            ),
-          ],
-        ),
-      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
 
-class SampleBind extends Bindings {
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
   @override
-  void dependencies() {
-    Get.lazyPut<ControllerX>(() => ControllerX());
-  }
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class User {
-  User({this.name = 'Name', this.age = 0});
-  String name;
-  int age;
-}
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
 
-class ControllerX extends GetxController {
-  final count1 = 0.obs;
-  final count2 = 0.obs;
-  final list = [56].obs;
-  final user = User().obs;
-
-  updateUser() {
-    user.update((value) {
-      value!.name = 'Jose';
-      value.age = 30;
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
     });
   }
 
-  /// Once the controller has entered memory, onInit will be called.
-  /// It is preferable to use onInit instead of class constructors or initState method.
-  /// Use onInit to trigger initial events like API searches, listeners registration
-  /// or Workers registration.
-  /// Workers are event handlers, they do not modify the final result,
-  /// but it allows you to listen to an event and trigger customized actions.
-  /// Here is an outline of how you can use them:
-
-  /// made this if you need cancel you worker
-  late Worker _ever;
-
   @override
-  onInit() {
-    /// Called every time the variable $_ is changed
-    _ever = ever(count1, (_) => print("$_ has been changed (ever)"));
-
-    everAll([count1, count2], (_) => print("$_ has been changed (everAll)"));
-
-    /// Called first time the variable $_ is changed
-    once(count1, (_) => print("$_ was changed once (once)"));
-
-    /// Anti DDos - Called every time the user stops typing for 1 second, for example.
-    debounce(count1, (_) => print("debouce$_ (debounce)"),
-        time: Duration(seconds: 1));
-
-    /// Ignore all changes within 1 second.
-    interval(count1, (_) => print("interval $_ (interval)"),
-        time: Duration(seconds: 1));
-  }
-
-  int get sum => count1.value + count2.value;
-
-  increment() => count1.value++;
-
-  increment2() => count2.value++;
-
-  disposeWorker() {
-    _ever.dispose();
-    // or _ever();
-  }
-
-  incrementList() => list.add(75);
-}
-
-class SizeTransitions extends CustomTransition {
-  @override
-  Widget buildTransition(
-      BuildContext context,
-      Curve? curve,
-      Alignment? alignment,
-      Animation<double> animation,
-      Animation<double> secondaryAnimation,
-      Widget child) {
-    return Align(
-      alignment: Alignment.center,
-      child: SizeTransition(
-        sizeFactor: CurvedAnimation(
-          parent: animation,
-          curve: curve!,
-        ),
-        child: child,
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
       ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug painting" (press "p" in the console, choose the
+          // "Toggle Debug Paint" action from the Flutter Inspector in Android
+          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+          // to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
