0d28d10bf913dd8bca1bbcbd9a006ef123d55785
Havasi Gabor <havasi.gabor@ursys.net>
2022-05-23 21:02:41 +0200

    fix who are you tests

--- test/groups/join_group/who_are_you_test.dart
+++ test/groups/join_group/who_are_you_test.dart
@@ -1,7 +1,10 @@
 import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:provider/provider.dart';
+import 'package:spending_share/models/user.dart';
 import 'package:spending_share/ui/groups/who_are_you.dart';
+import 'package:spending_share/utils/globals.dart' as globals;
 
 import '../../wrapper.dart';
 
@@ -27,56 +30,90 @@ void main() {
       });
 
       testWidgets('WhoAreYou has Who are you header', (WidgetTester tester) async {
-        var testWidget = testableWidget(child: WhoAreYou(firestore: firestore, groupId: 'group', color: Colors.orange));
+        var testWidget = testableWidget(
+            child: Provider(
+                create: (context) => SpendingShareUser(
+                  groups: [],
+                  categoryData: [],
+                  color: globals.colors['default']!,
+                  icon: globals.icons['default']!,
+                ),
+                child: WhoAreYou(firestore: firestore, groupId: 'group', color: Colors.orange)));
         await tester.pumpWidget(testWidget);
         final textFinder = find.text('who_are_you');
         expect(textFinder, findsOneWidget);
       });
-
-      /*
-      testWidgets('WhoAreYou has member list', (WidgetTester tester) async {
-        print('asd');
-        var testWidget = testableWidget(child: WhoAreYou(firestore: firestore, group: group));
-        await tester.pumpWidget(testWidget);
-        await tester.pumpAndSettle();
-
-        // TODO
-
-        final textFinder = find.text('who_are_you');
-        expect(textFinder, findsOneWidget);
-      });
-       */
 
       testWidgets('WhoAreYou has back button', (WidgetTester tester) async {
-        var testWidget = testableWidget(child: WhoAreYou(firestore: firestore, groupId: 'group', color: Colors.orange));
+        var testWidget = testableWidget(
+            child: Provider(
+                create: (context) => SpendingShareUser(
+                  groups: [],
+                  categoryData: [],
+                  color: globals.colors['default']!,
+                  icon: globals.icons['default']!,
+                ),
+                child: WhoAreYou(firestore: firestore, groupId: 'group', color: Colors.orange)));
         await tester.pumpWidget(testWidget);
         final textFinder = find.text('back');
         expect(textFinder, findsOneWidget);
       });
 
       testWidgets('WhoAreYou has none of these text', (WidgetTester tester) async {
-        var testWidget = testableWidget(child: WhoAreYou(firestore: firestore, groupId: 'group', color: Colors.orange));
+        var testWidget = testableWidget(
+            child: Provider(
+                create: (context) => SpendingShareUser(
+                  groups: [],
+                  categoryData: [],
+                  color: globals.colors['default']!,
+                  icon: globals.icons['default']!,
+                ),
+                child: WhoAreYou(firestore: firestore, groupId: 'group', color: Colors.orange)));
         await tester.pumpWidget(testWidget);
         final textFinder = find.text('none_of_these');
         expect(textFinder, findsOneWidget);
       });
 
       testWidgets('WhoAreYou has join input field', (WidgetTester tester) async {
-        var testWidget = testableWidget(child: WhoAreYou(firestore: firestore, groupId: 'group', color: Colors.orange));
+        var testWidget = testableWidget(
+            child: Provider(
+                create: (context) => SpendingShareUser(
+                  groups: [],
+                  categoryData: [],
+                  color: globals.colors['default']!,
+                  icon: globals.icons['default']!,
+                ),
+                child: WhoAreYou(firestore: firestore, groupId: 'group', color: Colors.orange)));
         await tester.pumpWidget(testWidget);
         final joinInputField = find.byKey(const Key('join_input_field'));
         expect(joinInputField, findsOneWidget);
       });
 
       testWidgets('WhoAreYou has join button', (WidgetTester tester) async {
-        var testWidget = testableWidget(child: WhoAreYou(firestore: firestore, groupId: 'group', color: Colors.orange));
+        var testWidget = testableWidget(
+            child: Provider(
+                create: (context) => SpendingShareUser(
+                  groups: [],
+                  categoryData: [],
+                  color: globals.colors['default']!,
+                  icon: globals.icons['default']!,
+                ),
+                child: WhoAreYou(firestore: firestore, groupId: 'group', color: Colors.orange)));
         await tester.pumpWidget(testWidget);
         final joinButton = find.byKey(const Key('join_button'));
         expect(joinButton, findsOneWidget);
       });
 
       testWidgets('WhoAreYou has bottom navigation', (WidgetTester tester) async {
-        var testWidget = testableWidget(child: WhoAreYou(firestore: firestore, groupId: 'group', color: Colors.orange));
+        var testWidget = testableWidget(
+            child: Provider(
+                create: (context) => SpendingShareUser(
+                  groups: [],
+                  categoryData: [],
+                  color: globals.colors['default']!,
+                  icon: globals.icons['default']!,
+                ),
+                child: WhoAreYou(firestore: firestore, groupId: 'group', color: Colors.orange)));
         await tester.pumpWidget(testWidget);
         final bottomNavigationFinder = find.byKey(const Key('bottom_navigation'));
         expect(bottomNavigationFinder, findsOneWidget);
